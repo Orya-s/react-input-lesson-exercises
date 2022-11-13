7b7687251aeb33e3a37073a2eb8036bca22ff479
orya <orya.sp@gmail.com>
2022-11-13 17:11:39 +0200

    Add my solution

--- src/components/Exercise1.js
+++ src/components/Exercise1.js
@@ -10,12 +10,21 @@ class Exercise1 extends Component {
         }
     }
 
+    alertText = () => alert(`Come in ${this.state.name}, your'e ${this.state.age} - that's good enough`)
+
+    updateName = e => {
+        this.setState({name: e.target.value})
+    } 
+    updateAge = e => {
+        this.setState({age: e.target.value})
+    } 
+
     render() {
         return (
             <div>
-                <input id="name-input" />
-                <input id="age-input" />
-                <button>Go to Bar</button>
+                <input id="name-input" placeholder='Your name' value={this.state.name} onChange={this.updateName}/>
+                <input id="age-input" placeholder='Your age' value={this.state.age} onChange={this.updateAge}/>
+                <button onClick={this.alertText}>Go to Bar</button>
             </div>
         );
     }
